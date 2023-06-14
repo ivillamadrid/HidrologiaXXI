5960ef21f2a814eac09ad12ddc742d8cb0e53853
ivillamadrid <32260988+ivillamadrid@users.noreply.github.com>
2023-06-14 14:29:32 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Hidrología-XXI'
+copyright = '2023, IHLLA'
+author = 'ivillanueva'
 
 release = '0.1'
 version = '0.1.0'
